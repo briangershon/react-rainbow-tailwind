fa25562dbf88e2032ee80e25ffe0767d44c4aadb
Brian Gershon <briangershon@users.noreply.github.com>
2022-12-11 18:39:53 -0800

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function App() {
   return (
-    <div className="flex p-4 items-center justify-center">
-      <div className="flex-1">
-        <h1 className="text-3xl font-bold">Hello</h1>
-      </div>
-      <div className="flex-0">
-        <ConnectButton />
-      </div>
+    <div className="max-w-2xl mx-auto">
+      <header className="bg-base-200 h-40">
+        <div className="flex p-4 items-center justify-center">
+          <div className="flex-1"></div>
+          <div className="flex-0">
+            <ConnectButton />
+          </div>
+        </div>
+        <h1 className="text-3xl font-bold text-center mt-4">Heading</h1>
+      </header>
+      <main className="text-center mt-6">Content</main>
     </div>
   );
 }
